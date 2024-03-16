7cabf3c33ced0c1c1614d08b34b2699fadeb0f45
JaswanthSudha <210304124462@paruluniversity.ac.in>
2024-03-16 08:35:50 +0530

    first commit

---
+++ backend/controlllers/workoutController.js
@@ -0,0 +1,71 @@
+const mongoose=require("mongoose")
+const Workout=require("../models/workoutModel")
+
+//Get All the Documents
+const getWorkouts=async(req,res)=>{
+    const workouts= await Workout.find({}).sort({createdAt:-1});
+    res.status(200).json(workouts);
+}
+//Get a Single Document
+const getWorkOut=async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No Such Workout"});
+    }
+    const workout=await Workout.findById(id);
+    if(!workout){
+        return res.status(404).json({error:"No Such Workout"});
+
+    }
+    res.status(200).json(workout)
+}
+//create a new Workout
+const createWorkout=async(req,res)=>{
+    const {title,load,reps}=req.body;
+    try{
+        const workout=await Workout.create({title,load,reps});
+        res.status(200).json(workout);
+    }
+    catch(error){
+        res.status(400).json({err:"Some Error"});
+    }
+}
+//delete a workOut
+const deleteWorkout=async(req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No Such Workout"});
+    }
+    const workout=await Workout.findOneAndDelete({_id:id});
+    if(!workout){
+        return res.status(404).json({error:"No Such Workout"});
+
+    }
+    res.status(200).json({msg:"Delted",workout:workout})
+
+
+}
+//update a WorkOut
+const updateWorkOut=async (req,res)=>{
+    const {id}=req.params;
+    if(!mongoose.Types.ObjectId.isValid(id)){
+        return res.status(404).json({error:"No Such Workout"});
+    }
+    const workout=await Workout.findOneAndUpdate({_id:id},{
+        ...req.body
+    })
+    if(!workout){
+        return res.status(404).json({error:"No Such Workout"});
+
+    }
+    res.status(200).json({msg:"Updated",workout:workout})
+
+
+}
+module.exports={
+    getWorkOut,
+    getWorkouts,
+    createWorkout,
+    deleteWorkout,
+    updateWorkOut
+}
